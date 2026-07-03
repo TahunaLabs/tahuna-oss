f787cd1502d4474e4d12371824a79d38899a27f3
Pazuzzu <bogaforlife@gmail.com>
2026-07-03 18:49:55 +0200

    chore(docs): update CDN base url to tahuna account

--- docs/src/config.ts
+++ docs/src/config.ts
@@ -1,5 +1,5 @@
 export const CDN_CONFIG = {
-  baseUrl: "https://pub-5f7c225fd68446a9b652150d7c7e52e9.r2.dev",
+  baseUrl: "https://pub-558336c62f024020ac75182f00ba41b2.r2.dev",
   artefactsPath: "/assets/artefacts",
   faviconPath: "/assets/favicon",
 } as const
